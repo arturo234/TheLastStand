2572ef3cb7d6c0d450ac5c0875837f73e4e63603
Fierfek <geekssw@yahoo.com>
2015-02-21 16:17:28 -0800

    Revert "Generic enemy"
    This reverts commit 0aedbe137dfa8b22817f40403a9cfcd2bc65752a.

--- Assets/Scripts/GenericEnemyScript.cs
+++ Assets/Scripts/GenericEnemyScript.cs
@@ -3,36 +3,13 @@ using System.Collections;
 
 public class GenericEnemyScript : MonoBehaviour {
 
-	public double health, fireRate;
-	public float arrowVelocity;
-	public GameObject arrowPrefab;
-	double currentTime;
-
-	Vector3 theta, arrowDir;
-
 	// Use this for initialization
 	void Start () {
-		theta = new Vector3(0, 0, Random.value*360);
-		arrowDir = new Vector3 (Mathf.Cos(theta.z * Mathf.PI / 180), Mathf.Sin(theta.z * Mathf.PI / 180));
-		transform.Rotate(theta);
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		currentTime += Time.deltaTime;
-		if (currentTime >= fireRate) {
-			GameObject arrow = (GameObject)Instantiate(arrowPrefab, transform.position, transform.rotation);
-			arrow.rigidbody2D.velocity = arrowDir * arrowVelocity;
-			currentTime = 0;
-			//All the arrow prefab needs is a rigidBody2D with everything 0'd out.
-			//The arrow will only have to time out and kill itself.
-		}
-	}
-
-	void OnColliderEnter2D(Collider col) {
-		//code if we are not using object pools.
-		if(!col.tag.Equals("Enemy")) //Incase of non hero hits. 
-			Destroy(gameObject);
-		//Code if we are using object pools
+	
 	}
 }
