df74391361583767cf41099851763c899ff99d09
Mzatlin <michael.zatlin@yahoo.com>
2015-02-23 23:24:21 -0800

    Player Controller
    Player can now move with the WASD and arrow keys in 8 directions.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+
+public class PlayerController : MonoBehaviour{
+	public float walkSpeed = 3.5f; //movement speed of the player
 	
+	
+	void Start(){
+		
 	}
 	
-	// Update is called once per frame
-	void Update () {
 	
+	void Update(){
+		//WASD Controls
+		
+		if (Input.GetKey (KeyCode.D)) {
+			transform.position += new Vector3 (walkSpeed * Time.deltaTime, 0.0f, 0.0f);
+		} 
+		if (Input.GetKey (KeyCode.A)) {
+			transform.position -= new Vector3 (walkSpeed * Time.deltaTime, 0.0f, 0.0f);
+		} 
+		if (Input.GetKey (KeyCode.W)) {
+			transform.position += new Vector3 (0.0f,walkSpeed * Time.deltaTime, 0.0f);
+		} 
+		if (Input.GetKey (KeyCode.S)) {
+			transform.position -= new Vector3 (0.0f,walkSpeed * Time.deltaTime, 0.0f);
+		}
+		
+		//arrow key controls 
+		
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.position += new Vector3 (walkSpeed * Time.deltaTime, 0.0f, 0.0f);
+		} 
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.position -= new Vector3 (walkSpeed * Time.deltaTime, 0.0f, 0.0f);
+		} 
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.position += new Vector3 (0.0f,walkSpeed * Time.deltaTime, 0.0f);
+		} 
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.position -= new Vector3 (0.0f,walkSpeed * Time.deltaTime, 0.0f);
+		}
 	}
 }
