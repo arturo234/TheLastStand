7133e8cd9b1d42b2746b3e734b55d0bf0697a1bd
ARGutierrez <argutierrez92@gmail.com>
2015-03-02 00:11:34 -0800

    Cleaned up PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,61 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-
-
-public class PlayerController : MonoBehaviour{
-	public float walkSpeed = 3.5f; //movement speed of the player
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+	private float moveSpeed = 3.5f; // movement speed of the player
 	
-	
-	void Start(){
-		
+	void LateUpdate()
+    {
+        Move();
+        RotateToMouse();
 	}
-	
-	
-	void Update(){
-		//WASD Controls
-		Vector3 horizontal = new Vector3(walkSpeed * Time.deltaTime, 0.0f, 0.0f);//player's horizontal vector
-		Vector3 vertical = new Vector3(0.0f,walkSpeed * Time.deltaTime, 0.0f);//player's vertical vector
 
-		if (Input.GetKey (KeyCode.D)) {
-			transform.position += horizontal;
-		} 
-		if (Input.GetKey (KeyCode.A)) {
-			transform.position -= horizontal;
-		} 
-		if (Input.GetKey (KeyCode.W)) {
-			transform.position += vertical;
-		} 
-		if (Input.GetKey (KeyCode.S)) {
-			transform.position -= vertical;
-		}
-		
-		//arrow key controls 
-		
-		if (Input.GetKey (KeyCode.RightArrow)) {
-			transform.position += horizontal;
-		} 
-		if (Input.GetKey (KeyCode.LeftArrow)) {
-			transform.position -= horizontal;
-		} 
-		if (Input.GetKey (KeyCode.UpArrow)) {
-			transform.position += vertical;
-		} 
-		if (Input.GetKey (KeyCode.DownArrow)) {
-			transform.position -= vertical;
-		}
-	
-		//Mouse Aiming 
+    private void RotateToMouse()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+        Vector3 diff = Camera.main.ScreenToWorldPoint(mousePosition) - transform.position;
+        diff.Normalize();
 
-		Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 8);//mouse's position
+        float rotation = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotation);
+    }
 
-		Vector3 lookPosition = Camera.main.ScreenToWorldPoint(mousePosition);//player's position
+    private void Move()
+    {
 
-		lookPosition = lookPosition - transform.position;
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
 
-		float angle = Mathf.Atan2(lookPosition.y, lookPosition.x) * Mathf.Rad2Deg;
-		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
-	
-	}
+        Vector3 translate = new Vector3(h, v, 0);
+        translate = translate.normalized;
+        transform.position += translate * moveSpeed * Time.deltaTime;
+    }
 }
