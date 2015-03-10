b2170f21a9f8dcf7713e8d89ba240f09f8967310
Nesskittycat <Nessa@dhcp-33-4-243.dhcpint.csulb.edu>
2015-03-10 12:18:25 -0700

    This is my test branch
    Trying to commit.

--- Assets/Scripts/GenericEnemy.cs
+++ Assets/Scripts/GenericEnemy.cs
@@ -2,23 +2,21 @@
 using System.Collections;
 
 public class GenericEnemy : GenericCharacter {
-
+	Vector3 playerPosition, diff;
 	GameObject arrow;
+	float rotation;
 
-	// Use this for initialization
-	void Start () 
-    {
-		theta = new Vector3(0, 0, 0);//z value controls rotation, 0 is facing to the right
-		arrowDir = new Vector3 (Mathf.Cos(theta.z * Mathf.PI / 180), Mathf.Sin(theta.z * Mathf.PI / 180));
-		transform.Rotate(theta);
-	}
 
 	// Update is called once per frame
 	void Update () 
-    {
+	{
+		RotateToPlayer ();
+		theta = new Vector3(0, 0, rotation);//z value controls rotation, 0 is facing to the right
+		arrowDir = new Vector3 (Mathf.Cos(theta.z * Mathf.PI / 180), Mathf.Sin(theta.z * Mathf.PI / 180));
 		currentTime += Time.deltaTime;
+		
 		if (currentTime >= fireRate) 
-        {
+		{
 			arrow = ObjectPool.instance.GetObjectForType("BasicProjectile", false);
 			arrow.transform.position = transform.position;
 			arrow.transform.rotation = transform.rotation;
@@ -27,25 +25,43 @@ public class GenericEnemy : GenericCharacter {
 			currentTime = 0;
 			//All the arrow prefab needs is a rigidBody2D with everything 0'd out.
 			//The arrow will only have to time out and kill itself.
-            
+			
 		}
 	}
-
+	
+	//Rotate to face a player
+	private void RotateToPlayer()
+	{
+		GameObject player = GameObject.Find("Player");
+		Transform playerTransform = player.transform;
+		// get player position
+		playerPosition = playerTransform.position;
+		playerPosition = new Vector3(playerPosition.x, playerPosition.y, 0);
+		diff = playerPosition - transform.position;
+		diff.Normalize();
+		rotation = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler(0f, 0f, rotation);
+		
+	}
+	
+	
 	public void OnTriggerEnter2D(Collider2D col) 
-    {
-        if (col.tag == "EnemyArrow") return;
+	{
+		if (col.tag == "EnemyArrow") return;
 		if (col.gameObject.tag.Equals("PlayerArrow")) 
 		{
 			health--;
-
+			
 			Destroy(col.gameObject);
 			//ObjectPool.instance.PoolObject(col.gameObject);
 			//causes arrow to stick, cleans up after enough arrows have been
 			//shot by player.
-            //col.rigidbody2D.velocity = Vector2.zero;
-
+			//col.rigidbody2D.velocity = Vector2.zero;
+			
 		}
 	}
-
-   
+	
+	
 }
+
+
