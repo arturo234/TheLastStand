aa3b9fe98563e3b900d482365d95080e2cbd82cc
Darkchicken <joshshucker@hotmail.com>
2015-03-10 22:45:49 -0700

    Minor Updates
    A few minor changes
    Beginning code for enemy line-of-sight(currently commented out)

--- Assets/Scripts/GenericEnemy.cs
+++ Assets/Scripts/GenericEnemy.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class GenericEnemy : GenericCharacter {
 	
 	GameObject arrow;
-	
 	// Use this for initialization
 	void Start () 
 	{
@@ -18,18 +17,29 @@ public class GenericEnemy : GenericCharacter {
 	void Update () 
 	{
 		currentTime += Time.deltaTime;
-		if (currentTime >= fireRate) 
-		{
-			arrow = ObjectPool.instance.GetObjectForType("BasicProjectile", false);
-			arrow.transform.position = transform.position;
-			arrow.transform.rotation = transform.rotation;
-			arrow.rigidbody2D.velocity = arrowDir * arrowVelocity;
-			arrow.tag = "EnemyArrow";
-			currentTime = 0;
-			//All the arrow prefab needs is a rigidBody2D with everything 0'd out.
-			//The arrow will only have to time out and kill itself.
-			
-		}
+		///the following code causes the enemy to only fire when the player is
+		/// in line of sight (work in progress)
+
+		//RaycastHit2D hit = Physics2D.Raycast (transform.position, -Vector2.up);
+		//if(hit.collider.tag == "Player") 
+		//{  
+			//attack code goes here
+			if (currentTime >= fireRate) 
+			{
+				arrow = ObjectPool.instance.GetObjectForType("BasicProjectile", false);
+				arrow.transform.position = transform.position;
+				arrow.transform.rotation = transform.rotation;
+				arrow.rigidbody2D.velocity = arrowDir * arrowVelocity;
+				arrow.tag = "EnemyArrow";
+				currentTime = 0;
+				//All the arrow prefab needs is a rigidBody2D with everything 0'd out.
+				//The arrow will only have to time out and kill itself.
+					
+			}
+
+		//}
+
+
 		if (health <= 0) 
 		{
 			Destroy(this.gameObject);
