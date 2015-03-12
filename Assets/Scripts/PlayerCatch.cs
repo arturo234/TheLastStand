6422b68e90aebec2c1adaf8ffc21de60191111f4
Darkchicken <joshshucker@hotmail.com>
2015-03-12 12:50:29 -0700

    Minor Control Fix
    Changed catch keyboard key from alt to Spacebar

--- Assets/Scripts/PlayerCatch.cs
+++ Assets/Scripts/PlayerCatch.cs
@@ -18,7 +18,7 @@ public class PlayerCatch : MonoBehaviour {
 
 	}
 	public void OnTriggerStay2D(Collider2D col){
-		//catches on "Fire2" press, which is right mouse button, alt, or gamepad button 1 (B on xbox 360 remote)
+		//catches on "Fire2" press, which is right mouse button, spacebar, or gamepad button 1 (B on xbox 360 remote)
 		if (col.gameObject.tag.Equals("EnemyArrow")&&(Input.GetButtonDown("Fire2")))//(Input.GetMouseButtonDown(1)||Input.GetKeyDown(KeyCode.LeftShift)))
 		{
 			if(playerScript.ammo < playerScript.ammoLimit )
