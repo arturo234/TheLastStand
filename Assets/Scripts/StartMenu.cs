5c27766ca4049607f592e04b4420b687311763f6
Darkchicken <joshshucker@hotmail.com>
2015-03-20 12:40:22 -0700

    Minor Fix

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -3,6 +3,18 @@ using System.Collections;
 
 public class StartMenu : MonoBehaviour {
 
+	public GameObject mobilePanel, pcPanel;
+
+	void Start () {
+		if (Application.isMobilePlatform) {
+			mobilePanel.SetActive(true);
+			pcPanel.SetActive(false);
+		} else {
+			mobilePanel.SetActive(false);
+			pcPanel.SetActive(true);
+		}
+
+	}
 	public void OnClickStart()
 	{
 		//Application.LoadLevel("BasicLevel");
