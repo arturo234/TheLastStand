5b6e1acb84f51a8933f6d9010738e8d2e089f648
Darkchicken <joshshucker@hotmail.com>
2015-03-20 12:33:56 -0700

    Animated Health Bar
    Health bar on lvl 5 is animated and functional.
    Added a temp background object to hide default bluescreen
    Made changes to title screen for android use

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour {
+	private Animator animator;
+	Player playerScript;
+	int health;
+	// Use this for initialization
+	void Start () {
+		animator = this.GetComponent<Animator>();
+		playerScript = GameObject.Find("Player").GetComponent<Player>();
+		//playerScript = transform.parent.GetComponent<Player>();
+		health = (int)playerScript.health;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		health = (int)playerScript.health;
+		animator.SetInteger("PlayerHealth", health);
+	}
+}
